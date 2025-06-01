bfcb1625e097a5cb84dbf211655d1a020202d83a
Marshanda Putri Ferdians <marshandaferdians02@gmail.com>
2025-06-01 13:25:57 +0700

    validation option

---
+++ src/util/ValidationOption.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author Marshanda Ferdians
+ */
+public class ValidationOption {
+
+    private boolean valid;
+    private String message;
+
+    public ValidationOption(boolean valid, String message) {
+        this.valid = valid;
+        this.message = message;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
